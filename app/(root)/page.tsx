db3810c42826d0839c4732a5cc3badca5ce8a40a
amlan-sahu <amlan9531@gmail.com>
2025-05-05 23:59:25 +0530

    feat: update package.json and add Sanity configuration
    - Removed turbopack from dev script in package.json.
    - Added predev and prebuild scripts for type generation.
    - Added new dependencies for Sanity and related plugins.
    - Created new Sanity configuration files: sanity.cli.ts, sanity.config.ts, and env.ts.
    - Added schema extraction and type generation configuration in sanity-typegen.json.
    - Implemented client and live content functionality in Sanity with new client.ts and live.ts files.
    - Defined queries for startups and authors in queries.ts.
    - Created document schemas for author and startup in schemaTypes.
    - Established content structure for Sanity Studio in structure.ts.
    - Generated types for Sanity schema in types.ts.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
 
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 
 export default async function Home({searchParams}: {searchParams: Promise<{query: string}>}) {
-  const query = (await searchParams).query || ""; 
+  const query = (await searchParams).query || "";
+
+  const params = {search: query|| null};
+
+  const session = await auth();
+
+
+
+  const {data:posts} = await sanityFetch({query:STARTUPS_QUERY, params});
+
+  
+  // const posts = [
+  //   {
+  //     _createdAt:new Date(),
+  //     views:55,
+  //     author:{ _id : 1, name:"Amlan"},
+  //     description:'This is a description',
+  //     image:'https://totempool.com/wp-content/uploads/2019/08/Startup_a4171014e8d4cfe295a3db794f5389d6_2000.jpg',
+  //     category:'Robots',
+  //     title:'We Robots'
+  //   }
+  // ]
   return (
     <>
         <section className="pink_container">
@@ -16,14 +42,18 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
 
         <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results for "${query}"` : "All Startups"}
+          {query ? `Search results for ${query}` : "All Startups"}
         </p>
 
         <ul className="mt-7 card_grid">
-          
+          {posts?.length > 0 ? (
+            posts.map((post:StartupTypeCard)=> (
+            <StartupCard key={post?._id} post={post}/>
+          ))
+          ):(<p className="no-results">No startup found</p>)}
         </ul>
         </section>
-    
+        <SanityLive/>
     </>
   );
 }
